731e1c526ce92b7fbfe85e91909ed6747c4dd02f
CaoRuis <1575825411@qq.com>
2022-08-19 10:43:37 +0800

    style:格式

--- app/src/main/java/com/test/systemapplication/MainActivity.java
+++ app/src/main/java/com/test/systemapplication/MainActivity.java
@@ -1,31 +1,32 @@
 package com.test.systemapplication;
 
-import android.content.Context;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.test.systemapplication.databinding.ActivityMainBinding;
+
+/**
+ * @author sm2886
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+        binding.confirm.setOnClickListener(this);
+    }
 
     @Override
     public void onClick(View v) {
 
         if (v.getId() == R.id.confirm) {
-
             Toast.makeText(this, "confirm onClick", Toast.LENGTH_LONG).show();
         }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btnConfirm = (Button) findViewById(R.id.confirm);
-        btnConfirm.setOnClickListener(this);
     }
 }
